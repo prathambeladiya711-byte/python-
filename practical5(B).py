f9ce51146db3747ba3bc4d7138269fb9ca815d3c
prathambeladiya711-byte <prathambeladiya711@gmail.com>
2026-09-08 12:09:20 +0530

    Refactor to use tuple for element storage
    Refactored code to use a tuple for storing elements instead of a list. Updated input prompts and print statements for clarity.

--- practical5(B).py
+++ practical5(B).py
@@ -1,19 +1,19 @@
-n = int(input("Enter the number of elements: "))
+#Practical 5.2
 
-elements = []
+n = int(input("Enter number of elements: "))
+
+t = ()
 
 for i in range(n):
-    value = input(f"Enter element {i + 1}: ")
-    elements.append(value)
+    x = int(input("Enter value: "))
+    t = t + (x,)
 
-my_tuple = tuple(elements)
+print("Tuple:", t)
 
-print("\nTuple:", my_tuple)
+# Using indexing
+print("First element:", t[0])
 
-print("\nAccessing elements using indexing:")
-for i in range(len(my_tuple)):
-    print("Index", i, ":", my_tuple[i])
-
-print("\nAccessing elements using looping:")
-for element in my_tuple:
-    print(element)
+# Using loop
+print("Tuple elements:")
+for x in t:
+    print(x)
